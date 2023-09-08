378322d42f10eda1467d158f7d92378607219f2e
Hemant <hrsmart2150@gmail.com>
2023-09-08 12:08:42 +0530

    I totally get Kafka now!

---
+++ admin.js
@@ -0,0 +1,24 @@
+const { kafka } = require("./client");
+
+async function init() {
+  const admin = kafka.admin();
+  console.log("Admin connecting..");
+  admin.connect();
+  console.log("Admin connected..");
+
+  console.log("Creating topic ['rider-updates']...");
+  await admin.createTopics({
+    topics: [
+      {
+        topic: "rider-updates",
+        numPartitions: 2,
+      },
+    ],
+  });
+  console.log("Topic created success ['rider-updates']");
+
+  console.log("Disconnecting admin..");
+  await admin.disconnect();
+}
+
+init();
